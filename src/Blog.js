6136437305c0ca6e23d3fec4c3a876f80f209082
ntBre <brentrwestbrook@gmail.com>
2019-07-25 17:25:08 -0500

    updated for judd

--- src/Blog.js
+++ src/Blog.js
@@ -1,8 +1,39 @@
 import React from "react"
+import { directive } from "@babel/types";
 
-function Blog() {
-    return (
-        <h1>Blog</h1>
-    )
+class Blog extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            display: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                display: !prevState.display
+            }
+        })
+    }
+    render() {
+        if (this.state.display === true) {
+            return (
+                <div>
+                    <button onClick={this.handleClick}>Toggle blog</button>
+                    <h1>Blog</h1>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <button onClick={this.handleClick}>Toggle blog</button>
+                    <h1></h1>
+                </div>
+            )
+        }
+    }
 }
 export default Blog
